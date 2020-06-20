13e01718cb74e18e0e3637c52e440c701e941839
jackdevelope <46283130+jackdevelope@users.noreply.github.com>
2020-06-20 09:59:06 +0800

    Delete StudentAgeException.java

--- javamode/src/com/company/Student0/Exception/StudentAgeException.java
+++
@@ -1,7 +0,0 @@
-package com.company.Student0.Exception;
-
-import com.company.Student0.Exception.Impl.StudentAgeExceptionImpl;
-
-public interface StudentAgeException {
-    public void StudentAgeException() throws StudentAgeExceptionImpl;
-}
