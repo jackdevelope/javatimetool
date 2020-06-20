4ce9b5be81e825958a425df6e6a4d4f63a495583
tom <timecount@163.com>
2020-06-20 10:02:11 +0800

    new

--- javamode/src/com/company/Student0/Exception/StudentIdExpection.java
+++
@@ -1,8 +0,0 @@
-package com.company.Student0.Exception;
-
-import com.company.Student0.Exception.Impl.StudentIdExceptionImpl;
-
-public interface StudentIdExpection {
-    public void StudentIdHanderException() throws StudentIdExceptionImpl;
-
-}
